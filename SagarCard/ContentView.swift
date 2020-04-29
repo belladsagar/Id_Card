540206e9273e21aa5a06632f4f51f64a3afd2d1e
SAGAR C BELLAD <scbellad2025@gmail.com>
2020-04-29 23:56:46 +0530

    Initial Commit

---
+++ SagarCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SagarCard
+//
+//  Created by Sagar c bellad on 29/04/20.
+//  Copyright © 2020 Sagar c bellad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
