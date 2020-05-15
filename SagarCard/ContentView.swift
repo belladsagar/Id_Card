0c8e59b150af8001c10d745579870c3d9c934a3d
SAGAR C BELLAD <scbellad2025@gmail.com>
2020-05-15 22:32:06 +0530

    Final commit

--- SagarCard/ContentView.swift
+++ SagarCard/ContentView.swift
@@ -6,11 +6,48 @@
 //  Copyright © 2020 Sagar c bellad. All rights reserved.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52,opacity: 1.0).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Image("myPic").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center).clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 5))
+
+                Text("Sagar c Bellad")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                
+                ExtractedView(value: "+91 6361842210", photoKey: "phone.fill")
+                ExtractedView(value: "scbellad2025@gmail.com", photoKey: "envelope.fill")
+            }
+        }
+    }
+}
+
+struct ExtractedView: View {
+    let value: String
+    let photoKey: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 40).frame(width: 412, height: 50)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: photoKey).foregroundColor(.green)
+                Text(value)
+            })
+            .padding(.all)
     }
 }
 
@@ -19,3 +56,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//phone.fill
+//+91 6361842210
